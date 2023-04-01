72ccbf8f06e32ffddac5697d2f5a4d08cb838b74
curious-toast <45948951+curious-toast@users.noreply.github.com>
2023-03-31 23:54:43 -0400

    Add files via upload

---
+++ lua/user/plugins.lua
@@ -0,0 +1,10 @@
+lvim.plugins = {
+  "windwp/nvim-spectre",
+  "gpanders/editorconfig.nvim",
+  "VonHeikemen/searchbox.nvim",
+  "MunifTanjim/nui.nvim",
+--     {
+--       "folke/trouble.nvim",
+--       cmd = "TroubleToggle",
+--     },
+}
